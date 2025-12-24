c8d24c382200856fd0d2be791aa22129335ba325
tarhaneda <tarhanedanur1@gmail.com>
2025-12-24 19:29:02 +0300

    update

--- mekanbul-frontend-main/src/services/http-common.jsx
+++ mekanbul-frontend-main/src/services/http-common.jsx
@@ -9,7 +9,7 @@ export default axios.create({
   // Tüm API istekleri bu URL'e göre yapılır
   // Örnek: baseURL = "http://localhost:3000/api" ise
   // GET isteği -> "http://localhost:3000/api/venues" olur
-  baseURL: "https://web-main-lilac.vercel.app/api",
+  baseURL: "https://mekanbul-liard.vercel.app/api",
 
   // HTTP istek başlıkları (headers)
   // Her istekte otomatik olarak bu başlıklar gönderilir
